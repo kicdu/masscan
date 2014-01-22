0c6f9e362b0a8724dc90573ee13fc367c8b6842d
Dalibor Dukic <dalibor.dukic@gmail.com>
2014-01-22 17:48:40 +0100

    NTP monlist Denial of Service Vulnerability check
        - initial commit

---
+++ src/proto-ntp-monlist.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "output.h"
+#include "proto-preprocess.h"
+#include "proto-banner1.h"
+
+/* NTP monlist
+
+https://cert.litnet.lt/en/docs/ntp-distributed-reflection-dos-attacks
+
+*/
+
+unsigned handle_ntp(struct Output *out, time_t timestamp, const unsigned char *px, unsigned length, struct PreprocessedInfo *parsed){
+
+    // TODO: handle_ntp
+    return 0;
+}
+
+unsigned ntp_monlist(){
+
+    return 0;
+}
+
+
+
---
+++ src/proto-ntp-monlist.h
@@ -0,0 +1,8 @@
+#ifndef PROTO_NTPMONLIST_H
+#define PROTO_NTPMONLIST_H
+
+unsigned handle_ntp(struct Output *, time_t , const unsigned char *, unsigned , struct PreprocessedInfo *);
+
+//unsigned ntp_monlist();
+
+#endif
